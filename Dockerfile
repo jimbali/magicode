b6941a7ab6d13fdc1f810472247577e8f6905c5f
Jim Bailey <magicode@outlook.com>
2020-07-12 11:54:20 +0100

    Fix the contact form

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,21 @@
 FROM php:7.4-apache
+
+RUN apt-get update
+RUN apt-get install -y zip
+
+RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+
+RUN curl -sS https://getcomposer.org/installer | php -- \
+                                                     --install-dir=/usr/bin \
+                                                     --filename=composer
+
 COPY ./magicode.co.uk /var/www/html/
+
+WORKDIR /var/www/html/
+
+RUN composer install
+
 COPY ./exttest.magicode.co.uk /var/www/exttest.magicode.co.uk/
 COPY ./apache/exttest.magicode.co.uk.conf /etc/apache2/sites-available/
+
 RUN a2ensite exttest.magicode.co.uk
--- magicode.co.uk/_include/php/contact.php
+++ magicode.co.uk/_include/php/contact.php
@@ -2,27 +2,31 @@
 /*
 * Contact Form Class
 */
-
+require_once '/var/www/html/vendor/autoload.php';
 
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$admin_email = 'magicode@outlook.com'; // Your Email
+$admin_email = getenv('MAIL_TO'); // Your Email
 $message_min_length = 5; // Min Message Length
 
 
 class Contact_Form{
 	function __construct($details, $email_admin, $message_min_length){
-		
+
 		$this->name = stripslashes($details['name']);
 		$this->email = trim($details['email']);
-		$this->subject = 'Contact from Your Website'; // Subject 
+		$this->subject = 'Contact from Your Website'; // Subject
 		$this->message = stripslashes($details['message']);
-	
+
 		$this->email_admin = $email_admin;
 		$this->message_min_length = $message_min_length;
-		
+		$this->smtp_username = getenv('MAIL_USERNAME');
+		$this->smtp_password = getenv('MAIL_PASSWORD');
+		$this->smtp_host = getenv('MAIL_HOST');
+		$this->smtp_port = getenv('MAIL_PORT');
+
 		$this->response_status = 1;
 		$this->response_html = '';
 	}
@@ -30,13 +34,13 @@ class Contact_Form{
 
 	private function validateEmail(){
 		$regex = '/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i';
-	
-		if($this->email == '') { 
+
+		if($this->email == '') {
 			return false;
 		} else {
 			$string = preg_replace($regex, '', $this->email);
 		}
-	
+
 		return empty($string) ? true : false;
 	}
 
@@ -55,14 +59,14 @@ class Contact_Form{
 			$this->response_html .= '<p>Please enter an e-mail address</p>';
 			$this->response_status = 0;
 		}
-		
+
 		// Check valid email
 		if($this->email && !$this->validateEmail())
 		{
 			$this->response_html .= '<p>Please enter a valid e-mail address</p>';
 			$this->response_status = 0;
 		}
-		
+
 		// Check message length
 		if(!$this->message || strlen($this->message) < $this->message_min_length)
 		{
@@ -73,9 +77,20 @@ class Contact_Form{
 
 
 	private function sendEmail(){
-		$mail = mail($this->email_admin, $this->subject, $this->message . "\n\nFrom: " . $this->email);
-	
-		if($mail)
+		$transport = (new Swift_SmtpTransport($this->smtp_host, $this->smtp_port, 'tls'))
+		  ->setUsername($this->smtp_username)
+		  ->setPassword($this->smtp_password);
+
+		$mailer = new Swift_Mailer($transport);
+
+		$message = (new Swift_Message($this->subject))
+		  ->setFrom([$this->email => $this->name])
+		  ->setTo($this->email_admin)
+		  ->setBody($this->message);
+
+		$result = $mailer->send($message);
+
+		if($result > 0)
 		{
 			$this->response_status = 1;
 			$this->response_html = '<p>Thank You!</p>';
@@ -85,15 +100,16 @@ class Contact_Form{
 
 	function sendRequest(){
 		$this->validateFields();
+
 		if($this->response_status)
 		{
 			$this->sendEmail();
 		}
 
 		$response = array();
-		$response['status'] = $this->response_status;	
+		$response['status'] = $this->response_status;
 		$response['html'] = $this->response_html;
-		
+
 		echo json_encode($response);
 	}
 }
